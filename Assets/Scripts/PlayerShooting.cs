8f70a605e8125b15528a03a877ed478c9e33c964
joetinoco <jose.tinoco@gmail.com>
2016-11-19 15:13:03 -0500

    Terrain, mazes & gun

---
+++ Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShooting : MonoBehaviour {
+
+	public AudioSource LaserSound;
+	public GameObject MuzzleFlash;
+	public Transform FlashPoint;
+	public GameObject LaserImpact;
+	public Transform PlayerCamera;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetButtonDown("Fire1")) {
+			
+			Instantiate(this.MuzzleFlash, this.FlashPoint.position, Quaternion.identity);
+
+			RaycastHit hit;
+
+			if (Physics.Raycast(this.PlayerCamera.position, this.PlayerCamera.forward, out hit)) {
+				// if (hit.transform.gameObject.CompareTag("Enemy")) {
+				// 	Destroy (hit.transform.gameObject);
+				// } else {
+					GameObject sparks = (GameObject)Instantiate(LaserImpact, hit.point, Quaternion.identity);
+					Destroy(sparks, 5);
+				// }
+			}
+
+			LaserSound.Play();
+
+		}
+	}
+}
---
+++ Assets/Scripts/SparksBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SparksBehaviour : MonoBehaviour {
+
+	public float EmitterDuration;
+	public ParticleSystem ParticleSystem;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		EmitterDuration -= Time.deltaTime;
+		if (EmitterDuration <= 0) {
+			ParticleSystem.Stop();
+		}
+	
+	}
+}
